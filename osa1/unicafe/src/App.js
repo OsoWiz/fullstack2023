3500823671bda0b3c34f00579de5d1fecba9e3d2
otso <otso.laasonen@aalto.fi>
2023-01-23 15:47:04 +0200

    Osa 1.6 done

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,45 @@
+import { React, useState } from "react";
+
+const SimpleHeader = (props) => {
+  return <h1>{props.headerText}</h1>;
+};
+
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+function App() {
+  const title = "Give feedback";
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const giveGood = () => () => {
+    setFeedback({ ...feedback, good: feedback.good + 1 });
+  };
+
+  const giveNeutral = () => () => {
+    setFeedback({ ...feedback, neutral: feedback.neutral + 1 });
+  };
+
+  const giveBad = () => () => {
+    setFeedback({ ...feedback, bad: feedback.bad + 1 });
+  };
+
+  return (
+    <div>
+      <SimpleHeader headerText={title} />
+      <Button handleClick={giveGood()} text="good" />
+      <Button handleClick={giveNeutral()} text="neutral" />
+      <Button handleClick={giveBad()} text="bad" />
+      <SimpleHeader headerText="Statistics" />
+      <p>good: {feedback.good}</p>
+      <p>neutral: {feedback.neutral}</p>
+      <p>bad: {feedback.bad}</p>
+    </div>
+  );
+}
+
+export default App;
