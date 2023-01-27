da953e973576c7d1ab9a4317f1afc4992cd9d6f1
otso <otso.laasonen@aalto.fi>
2023-01-27 14:56:35 +0200

    Osa 2.4 tehty

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -43,36 +43,59 @@ const Course = ({ course }) => {
   );
 };
 
+const CourseList = ({ courses }) => {
+  return (
+    <>
+      {courses.map((course, i) => (
+        <Course key={i} course={course} />
+      ))}
+    </>
+  );
+};
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-      },
-      {
-        name: "Redux 2, the revenge",
-        exercises: 25,
-      },
-    ],
-  };
-
+  const courses = [
+    {
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+        },
+        {
+          name: "Redux 2, the revenge",
+          exercises: 25,
+        },
+      ],
+    },
+    {
+      name: "Big Data engineering",
+      parts: [
+        {
+          name: "Big Data",
+          exercises: 10,
+        },
+        {
+          name: "Stranger datasets",
+          exercises: 7,
+        },
+      ],
+    },
+  ];
   return (
     <div>
-      <Course course={course} />
+      <CourseList courses={courses} />
     </div>
   );
 };
