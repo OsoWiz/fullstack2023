c0a701c3ebea3cae19892449a90e56f08593d0f0
otso <otso.laasonen@aalto.fi>
2023-02-01 17:04:56 +0200

    Osa 2.6 done

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    const nameObject = { name: newName };
+    setPersons(persons.concat(nameObject));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name:{" "}
+          <input
+            type="text"
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, i) => (
+          <li key={i}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
---
+++ osa2/puhelinluettelo/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = document.getElementById("root");
+const root2 = ReactDOM.createRoot(root);
+root2.render(<App />);
