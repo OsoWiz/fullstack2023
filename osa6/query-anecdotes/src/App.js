5ee907994f03e43340251910010e226e37614605
otso <otso.laasonen@aalto.fi>
2023-07-12 16:20:22 +0300

    Osat 6.23-6.24 done

--- osa6/query-anecdotes/src/App.js
+++ osa6/query-anecdotes/src/App.js
@@ -1,18 +1,43 @@
 import { useQuery, useMutation, useQueryClient } from "react-query";
 import { getAnecdotes, updateAnecdote } from "./services/requests";
+import { useReducer } from "react";
+import NotificationContext from "./NotificationContext";
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
 
+const NotificationReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_NOTIFICATION":
+      return action.payload;
+    case "CLEAR_NOTIFICATION":
+      return null;
+    default:
+      return state;
+  }
+};
+
 const App = () => {
   const client = useQueryClient();
+  const [notification, notificationDispatch] = useReducer(
+    NotificationReducer,
+    null
+  );
   const updateMutation = useMutation(updateAnecdote, {
     onSuccess: () => client.invalidateQueries("anecdotes"),
   });
+
   const handleVote = (anecdote) => {
     updateMutation.mutate({
       ...anecdote,
       votes: anecdote.votes + 1,
     });
+    notificationDispatch({
+      type: "SET_NOTIFICATION",
+      payload: `Anecdote ${anecdote.content} voted`,
+    });
+    setTimeout(() => {
+      notificationDispatch({ type: "CLEAR_NOTIFICATION" });
+    }, 5000);
   };
 
   const res = useQuery("anecdotes", getAnecdotes);
@@ -23,22 +48,24 @@ const App = () => {
   const anecdotes = res.data;
 
   return (
-    <div>
-      <h3>Anecdote app</h3>
+    <NotificationContext.Provider value={[notification, notificationDispatch]}>
+      <div>
+        <h3>Anecdote app</h3>
 
-      <Notification />
-      <AnecdoteForm />
+        <Notification />
+        <AnecdoteForm />
 
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
+        {anecdotes.map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => handleVote(anecdote)}>vote</button>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </NotificationContext.Provider>
   );
 };
 
--- osa6/query-anecdotes/src/components/AnecdoteForm.js
+++ osa6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,16 +1,36 @@
 import { createAnecdote } from "../services/requests";
 import { useMutation, useQueryClient } from "react-query";
+import { useContext } from "react";
+import NotificationContext from "../NotificationContext";
 const AnecdoteForm = () => {
   const client = useQueryClient();
+  const [notification, notificationDispatch] = useContext(NotificationContext);
   const newAnecdoteMutation = useMutation(createAnecdote, {
-    onSuccess: () => client.invalidateQueries("anecdotes"),
+    onSuccess: (data) => {
+      client.invalidateQueries("anecdotes");
+      notificationDispatch({
+        type: "SET_NOTIFICATION",
+        payload: `Anecdote ${data.content} created`,
+      });
+      setTimeout(() => {
+        notificationDispatch({ type: "CLEAR_NOTIFICATION" });
+      }, 5000);
+    },
+    onError: (error) => {
+      notificationDispatch({
+        type: "SET_NOTIFICATION",
+        payload: `An error occurred: ${error.response.data.error}`,
+      });
+      setTimeout(() => {
+        notificationDispatch({ type: "CLEAR_NOTIFICATION" });
+      }, 5000);
+    },
   });
 
   const onCreate = (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
-    console.log("new anecdote");
     newAnecdoteMutation.mutate({ content, votes: 0 });
   };
 
